1655616819c90d4a07618baa69afebef8e001a0a
AntonioSabella <asabella6@gmail.com>
2022-03-30 10:55:25 +0200

    Initial commit Snack-2

---
+++ Snack-2/main.js
@@ -0,0 +1,22 @@
+/* SNACK 2 
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+ */
+
+const first_word = prompt('Inserisci la prima parola')
+const second_word = prompt('Inserisci la prima parola')
+
+
+//console.log(first_word.length);
+//console.log(second_word.length);
+
+const word1_lungezza = first_word.length;
+const word2_lungezza = second_word.length;
+
+if (word1_lungezza > word2_lungezza) {
+    console.log(word2_lungezza);
+    console.log(word1_lungezza);
+} else if (word2_lungezza > word1_lungezza) {
+    console.log(word1_lungezza);
+    console.log(word2_lungezza);
+} 
