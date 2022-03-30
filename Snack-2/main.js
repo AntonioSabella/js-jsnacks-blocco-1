d92c4f1f16a32bdd62574b9fa0e13ee5b534b1ed
AntonioSabella <asabella6@gmail.com>
2022-03-30 16:48:24 +0200

    Add adjust Snack-2

--- Snack-2/main.js
+++ Snack-2/main.js
@@ -3,22 +3,24 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
  */
 
-const first_word = prompt('Inserisci la prima parola')
-const second_word = prompt('Inserisci la prima parola')
+const first_word = prompt('Inserisci la prima parola');
+const second_word = prompt('Inserisci la prima parola');
 
 
 //console.log(first_word.length);
 //console.log(second_word.length);
 
-const word1_lungezza = first_word.length;
-const word2_lungezza = second_word.length;
+const word1_lunghezza = first_word.length;
+const word2_lunghezza = second_word.length;
 
-if (word1_lungezza > word2_lungezza) {
-    console.log(word2_lungezza);
-    console.log(word1_lungezza);
-} else if (word2_lungezza > word1_lungezza) {
-    console.log(word1_lungezza);
-    console.log(word2_lungezza);
-} else if (typeof word1_lungezza  == 'number' && typeof word2_lungezza == 'number') {
+if (word1_lunghezza > word2_lunghezza) {
+    console.log(word2_lunghezza);
+    console.log(word1_lunghezza);
+} else if (word2_lunghezza > word1_lunghezza) {
+    console.log(word1_lunghezza);
+    console.log(word2_lunghezza);
+} else if (word1_lunghezza == word2_lunghezza) {
+    console.log('Le due parole sono uguali');
+} else if (typeof word1_lunghezza  == 'number' && typeof word2_lunghezza == 'number') {
     console.log('Attenzione: inserire solo parole');
-}
+} 
