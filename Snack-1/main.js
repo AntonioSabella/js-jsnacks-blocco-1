d4eb6a255f7bdaf55e4823fd6d4afe0c0ff126b5
AntonioSabella <asabella6@gmail.com>
2022-03-30 10:21:54 +0200

    Initial commit Snack-1

---
+++ Snack-1/main.js
@@ -0,0 +1,12 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+
+const first_number = parseInt(prompt('Inserisci primo numero'))
+const second_number = parseInt(prompt('Inserisci secondo numero'))
+
+if (first_number > second_number) {
+    console.log(firs_number);
+} else if (second_number > first_number) {
+    console.log(second_number);
+}
